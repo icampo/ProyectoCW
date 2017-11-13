f6d51c398636886a5f32d6e36529508a8c91402b
icampo <icampope16dw@ikzubirimanteo.com>
2017-11-13 11:39:20 +0100

    Finalizado

--- resources/views/pages/herramienta.blade.php
+++ resources/views/pages/herramienta.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div id="animado" class="container">
-
+        <h3>Página en construcción</h3>
         @if (session('status'))
             <div class="alert alert-success">
                 {{ session('status') }}
