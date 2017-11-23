2d547cf3f1e9cfe697d02a57702c2309a8a1613e
icampo <icampope16dw@ikzubirimanteo.com>
2017-11-23 10:41:08 +0100

    Finalizado

--- resources/views/pages/herramienta.blade.php
+++ resources/views/pages/herramienta.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.default')
 
 @section('content')
+
     <div id="textoConst">
         <h3>Página en Construcción</h3>
     </div>
@@ -15,44 +16,5 @@
         <img src="/img/construccion1.gif">
 
     </div>
-
-    <script type="text/javascript">
-
-        let musica = new Audio("/img/ds.ogg");
-
-        function PulsarTecla(event){
-
-            tecla = event.code;
-
-            if(tecla==='KeyB'){
-
-                img = document.getElementById("animado");
-                imagen = '<img src="/img/ee.gif" alt="imagen" />';
-                img.innerHTML=imagen;
-                epi='url("/img/epi.gif")';
-                document.body.style.backgroundImage=epi;
-                musica.play();
-
-            }
-
-        }
-
-        window.onkeydown=PulsarTecla;
-
-        window.onkeyup=function parar(event) {
-
-            tecla = event.code;
-
-            if(tecla==='KeyB') {
-                img = document.getElementById("animado");
-                imagen = '<img src="/img/construccion1.gif" alt="imagen" />';
-                img.innerHTML=imagen;
-                document.body.style.backgroundColor="white";
-                document.body.style.backgroundImage=null;
-                musica.pause();
-            }
-        };
-
-    </script>
 
 @endsection
